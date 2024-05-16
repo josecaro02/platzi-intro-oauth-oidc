803a7cdae03265bdbf892864834d4967536c2eec
Jose David Caro Cantor <j.caro0207@gmail.com>
2024-05-16 17:14:40 -0500

    autenticación con cifrado asimetrico

--- sign-and-verifying/utils/token.js
+++ sign-and-verifying/utils/token.js
@@ -1,10 +1,13 @@
 // Load environment variables
 import * as dotenv from "dotenv";
 import jwt from 'jsonwebtoken'
+import fs from 'node:fs'
 dotenv.config();
 
 const  ONE_MINUTE_IN_MILISECONDS = 60 * 1000;
 const SECRET = process.env.SECRET
+const PRIVATE_KEY_PATH = process.env.PRIVATE_KEY_PATH
+const PUBLIC_KEY_PATH = process.env.PUBLIC_KEY_PATH
 
 export const signToken = (user) => {
   const payload = {
@@ -14,12 +17,20 @@ export const signToken = (user) => {
     exp: Date.now() + ONE_MINUTE_IN_MILISECONDS
   };
 
+  if(PRIVATE_KEY_PATH){
+    const privateKey = fs.readFileSync(PRIVATE_KEY_PATH, 'utf8')
+    return jwt.sign(payload, privateKey, {algorithm: 'RS256'})
+  }
   // TODO: Return signed token
   return jwt.sign(payload, SECRET)
   
 };
 
 export const verifyToken = (token) => {
+  if(PUBLIC_KEY_PATH){
+    const publicKey = fs.readFileSync(PUBLIC_KEY_PATH, 'utf8')
+    return jwt.verify(token, publicKey)
+  }
   return jwt.verify(token, SECRET);
 };
 
